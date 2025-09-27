1d2e4bca82559dde23010d04df828ab4b1e52622
Nillmen <162990067+Nillmen@users.noreply.github.com>
2025-09-27 09:05:14 +0700

    uploading window
    Tam uploaded

---
+++ gameSokoban/scripts/window.py
@@ -0,0 +1,29 @@
+import pygame
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = '1' 
+pygame.init()
+
+image_icon_path = r"assets\images\icon.png"
+
+class Window():
+    def __init__(self):
+        info = pygame.display.Info()
+        screen_w, screen_h = info.current_w, info.current_h
+        self.size = (screen_w, screen_h)
+        self.screen = pygame.display.set_mode(self.size)
+
+        self.caption = "Sokoban Game"
+        pygame.display.set_caption(self.caption)
+    
+        self.icon_ori = pygame.image.load(image_icon_path).convert_alpha()
+        self.icon = pygame.transform.scale(self.icon_ori, (32, 32))
+        pygame.display.set_icon(self.icon)
+
+        self.data = {}
+
+    def set_data(self, name, value):
+        self.data[name] = value
+
+    def get_data(self, name):
+        return self.data.get(name)
