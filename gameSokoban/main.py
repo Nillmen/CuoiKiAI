5bb04d1984c80cea1c59861da70bcbd422271c0f
Nillmen <162990067+Nillmen@users.noreply.github.com>
2025-10-15 07:37:33 +0700

    updating
    Tam updated

--- gameSokoban/main.py
+++ gameSokoban/main.py
@@ -1,7 +1,19 @@
+import sys
+from scripts import installLibrary
 from scripts import game
 
 def main():
-    Sokoban = game.start()
+    necessary_libs = {
+        "cv2" : "opencv_python", 
+        "pygame" : "pygame", 
+        "pygame_gui": "pygame_gui", 
+        "PIL" : "Pillow",
+        "openpyxl" : "openpyxl"
+    }
+    if installLibrary.check_and_install_libs(necessary_libs):
+        Sokoban = game.start()
+    else:
+        sys.exit()
     
 if __name__ == "__main__":
     main()
